f55fa6373513f3340be0e065ca7597cf12e622b7
Vitaliy Khyzhnyak <vit.khyzhn@gmail.com>
2025-08-18 03:59:37 +0300

    problems clean

--- app/notes/filter/[[...slug]]/page.tsx
+++ app/notes/filter/[[...slug]]/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import NotesPage from './NotesPage.client';
 
-type PageProps = {
-  params: { slug?: string[] };
-};
+type PageParams = { slug?: string[] };
 
-export default function NotesFilterPage({ params }: PageProps) {
-  const tag = Array.isArray(params.slug) ? params.slug[0] : undefined;
+export default async function NotesFilterPage({
+  params,
+}: {
+  params: Promise<PageParams>;
+}) {
+  const { slug } = await params;
+  const tag = Array.isArray(slug) ? slug[0] : undefined;
+
   return <NotesPage perPage={12} tag={tag} />;
 }
